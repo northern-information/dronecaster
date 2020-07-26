5ac9558805bcf52657bdbefbfa693bca8922841b
Tyler Etters <tyler@etters.co>
2020-07-26 16:59:49 -0500

    ++

--- dronecaster.lua
+++ dronecaster.lua
@@ -6,32 +6,64 @@
 --         k2: record  k3: cast
 --
 
-engine.name = 'Dronecaster'
+-- user configuration
+--------------------------------------------------------------------------------
+local filenamePrefix = "dronecaster_"
+
+
+
+-- engines
+--------------------------------------------------------------------------------
+
+engine.name = "Dronecaster"
+
+
 
 -- variables
 --------------------------------------------------------------------------------
 
 -- sounds
-local drones = {'Sine', 'Eno', 'Belong', 'Hecker', 'Gristle', 'Starlids', 'GY!BE', 'V/Vm', 'Canada'}
+local drones = {"Sine", "Eno", "Belong", "Hecker", "Gristle", "Starlids", "GY!BE", "V/Vm", "Canada"}
 local ampDefault = 0.02
 local recording = false
+local playing = false
+local filename = filenamePrefix
+
+
 
 -- ui
 local recordKey = false
 local castKey = false
+local alertX = 20
+local alertY = 40
+local alertW = 87
+local alertH = 12
+local alertRecording = false
+local startRecordingMessage = "Recording drone..."
+local stopRecordingMessage = "Drone saved!"
+local alertCasting = false
+local startCastingMessage = "Casting drone..."
+local stopCastingMessage = "Cast halted."
+
+
 
 -- draw
-local screenO = 0
 local screenL = 5
 local screenM = 10
 local screenH = 15
 
+
+
 -- time
 local seconds = 0
 local counter = metro.init()
+local recordingTime = 0
+
+
 
 -- animation
 local frame = 1
+local recordingFrame = 1
 local birdHomeX = 25
 local birdHomeY = 25
 local driftMinX = birdHomeX - 5
@@ -49,6 +81,10 @@ local unidentifiedAerialPhenomenon = false
 
 function init()
 
+  if util.file_exists(_path.audio .. "dronecaster/") == false then
+    util.make_dir(_path.audio .. "dronecaster/")
+  end
+
   screen.clear()
   screen.aa(0)
   screen.level(screenH)
@@ -57,21 +93,22 @@ function init()
 
   counter.time = 1
   counter.count = -1
+  counter.play = 1
   counter.event = theSandsOfTime
-  counter.play = 0
-
-  params:add_control("amp", "amp", controlspec.new(0, 1, 'amp', 0, ampDefault, 'amp'))
+  counter:start()
+      
+  params:add_control("amp", "amp", controlspec.new(0, 1, "amp", 0, ampDefault, "amp"))
   params:set_action("amp", function(x) updateAmp(x) end)
 
-  params:add_control("hz", "hz", controlspec.new(0, 20000, 'lin', 0, 440, 'hz'))
+  params:add_control("hz", "hz", controlspec.new(0, 20000, "lin", 0, 440, "hz"))
   params:set_action("hz", function(x) updateHz(x) end)
 
-  params:add_control("drone","drone",controlspec.new(1, 9, 'lin', 0, 1, 'drone'))
+  params:add_control("drone","drone",controlspec.new(1, 9, "lin", 0, 1, "drone"))
   params:set_action("drone", function(x) updateDrone(x) end)
 
   -- dev
-  counter:start()
-  counter.play = 1
+  -- key(3, 1) -- start
+  -- key(2, 1) -- record
 
 end
 
@@ -79,7 +116,13 @@ end
 
 function theSandsOfTime(c)
   seconds = c
-  frame = frame + 1
+  if playing then
+    frame = frame + 1  
+  end
+  if recording then
+    recordingFrame = recordingFrame + 1
+    recordingTime = recordingTime + 1
+  end
   redraw()
 end
 
@@ -104,7 +147,9 @@ function redraw()
   drawLandscape()
   drawClock()
   drawPlayStop()
-  drawTape()
+  drawRecording()
+  drawAlertRecording()
+  drawAlertCasting()
   screen.update()
 end
 
@@ -155,30 +200,44 @@ end
 
 
 
-function key(n,z)
+function key(n, z)
 
   -- k2 record
   if n == 2 and z == 1 then
+    
+    recording = not recording
+    
     if recording == true then
-      recording = false
+      units = 0
+      recordingTime = 0
+      alertRecordingFrame = 1
+      alertRecordingMessage = startRecordingMessage
+      alertRecording = true
     else
-      recording = true
+      alertRecordingFrame = 1
+      alertRecordingMessage = stopRecordingMessage
+      alertRecording = true
     end
+    
   end
 
   -- k3 cast
   if n == 3 and z == 1 then
-    if counter.play == 1 then
-      counter:stop()
-      counter.play = 0
-      frame = 1
+    
+    playing = not playing
+    
+    if playing == true then
       engine.startStop(0)
+      alertCastingFrame = 1
+      alertCastingMessage = startCastingMessage
+      alertCasting = true
     else
-      counter:start()
-      counter.play = 1
-      units = 0
       engine.startStop(1)
+      alertCastingFrame = 1
+      alertCastingMessage = stopCastingMessage
+      alertCasting = true
     end
+    
   end
 
   redraw()
@@ -219,6 +278,61 @@ end
 -- ui draws
 --------------------------------------------------------------------------------
 
+function drawAlertCasting()
+
+  if (alertCasting) then
+    drawAlertWindow()
+    drawAlertMessage(alertCastingMessage)
+    alertCastingFrame = alertCastingFrame + 1
+  end
+  
+  if (alertCastingFrame == 5) then
+    alertCasting = false
+    alertCastingFrame = false
+    alertCastingMessage = "..."
+  end
+
+end
+
+
+
+function drawAlertRecording()
+
+  if (alertRecording) then
+    drawAlertWindow()
+    drawAlertMessage(alertRecordingMessage)
+    alertRecordingFrame = alertRecordingFrame + 1
+  end
+  
+  if (alertRecordingFrame == 5) then
+    alertRecording = false
+    alertRecordingFrame = false
+    alertRecordingMessage = "..."
+  end
+
+end
+
+
+
+function drawAlertWindow()
+  screen.rect(alertX, alertY, alertW, alertH)
+  screen.level(screenH)
+  screen.stroke()
+  screen.rect(alertX, alertY, alertW - 1, alertH - 1)
+  screen.level(0)
+  screen.fill()
+end
+
+
+
+function drawAlertMessage(x)
+  screen.move((alertX + (alertW / 2)), (alertY + (alertH / 2) + 2))
+  screen.level(screenL)
+  screen.text_center(x)
+end
+
+
+
 function drawTopMenu()
 
   screen.level(screenL)
@@ -241,8 +355,8 @@ end
 
 function drawClock()
   screen.level(screenL)
-  screen.move(89, 64)
-  screen.text(util.s_to_hms(seconds))
+  screen.move(7, 64)
+  screen.text(util.s_to_hms(recordingTime))
 end
 
 
@@ -251,7 +365,7 @@ function drawPlayStop()
 
   screen.level(screenL)
 
-  if counter.play == 1 then
+  if playing == true then
     -- play
     mls(120, 59, 120, 64)
     mls(121, 60, 121, 63)
@@ -268,27 +382,26 @@ end
 
 
 
-function drawTape()
-  
-  -- reels
-  screen.circle(3, 61, 3)
-  screen.stroke()
-  screen.circle(13, 61, 3)
-  screen.stroke()
-  mls(3, 61, 1, 61)
-  mls(13, 61, 13, 64)
-  
-  -- tape
-  mlrs(6, 59, 2, 0)
-  mlrs(9, 59, 2, 0)
-  
-  
+function drawRecording()
+
+  screen.level(screenL)
+
   if recording then
-    -- todo
+    if (recordingFrame % 2) == 1 then
+      screen.circle(2, 61, 2)
+      screen.fill()
+    else
+      screen.circle(2, 61, 2)
+      screen.stroke()
+    end
   else
-    
+    screen.circle(2, 61, 2)
+    screen.stroke()
   end
+  
+
 end
+
 
 
 -- everything beyond is bird, tower, and wind animations...
@@ -298,36 +411,26 @@ function drawUAP()
   
   luck = math.random(0, 7)
 
-  if (luck == 3) and (unidentifiedAerialPhenomenon == false) then
+  if playing and (luck == 3) and (unidentifiedAerialPhenomenon == false) then
     unidentifiedAerialPhenomenon = true
     uapFrame = 1
   end
   
   if (unidentifiedAerialPhenomenon) then
     if uapFrame == 1 then
-      screen.move(100, 18)
-      screen.line(98, 20)
-      screen.stroke()
+      mls(100, 18, 98, 20)
       uapFrame = 2
     elseif uapFrame == 2 then
-      screen.move(100, 18)
-      screen.line(90, 25)
-      screen.stroke()
+      mls(100, 18, 90, 25)
       uapFrame = 3
     elseif uapFrame == 3 then
-      screen.move(94, 22)
-      screen.line(89, 26)
-      screen.stroke()
+      mls(94, 22, 89, 26)
       uapFrame = 4
     elseif uapFrame == 4 then
-      screen.move(88, 26)
-      screen.line(86, 28)
-      screen.stroke()
+      mls(88, 26, 86, 28)
       uapFrame = 5
     elseif uapFrame == 5 then
-      screen.move(85, 30)
-      screen.line_rel(1, 0)
-      screen.stroke()
+      mlrs(85, 30, 1, 0)
       unidentifiedAerialPhenomenon = false
       uapFrame = false
     end
@@ -382,7 +485,9 @@ function drawBirds()
 
   birdFrame = frame % 3
 
-  drift()
+  if playing then
+    drift()
+  end
 
   joeNowX = thisDriftX
   joeNowY = thisDriftY
@@ -394,74 +499,38 @@ function drawBirds()
 
   if birdFrame == 0 then
     -- joe
-    screen.move(joeNowX, joeNowY)
-    screen.line_rel(2, 2)
-    screen.stroke()
-    screen.move(joeNowX, joeNowY)
-    screen.line_rel(-2, 2)
-    screen.stroke()
+    mlrs(joeNowX, joeNowY, 2, 2)
+    mlrs(joeNowX, joeNowY, -2, 2)
     -- beth
-    screen.move(bethNowX, bethNowY)
-    screen.line_rel(2, -2)
-    screen.stroke()
-    screen.move(bethNowX, bethNowY)
-    screen.line_rel(-2, -2)
-    screen.stroke()
+    mlrs(bethNowX, bethNowY, 2, -2)
+    mlrs(bethNowX, bethNowY, -2, -2)
     -- alex
-    screen.move(alexNowX, alexNowY)
-    screen.line_rel(2, 1)
-    screen.stroke()
-    screen.move(alexNowX, alexNowY)
-    screen.line_rel(-2, 1)
-    screen.stroke()
+    mlrs(alexNowX, alexNowY, 2, 1)
+    mlrs(alexNowX, alexNowY, -2, 1)
   end
 
   if birdFrame == 1 then
     -- joe
-    screen.move(joeNowX, joeNowY)
-    screen.line_rel(2, 1)
-    screen.stroke()
-    screen.move(joeNowX, joeNowY)
-    screen.line_rel(-2, 1)
-    screen.stroke()
+    mlrs(joeNowX, joeNowY, 2, 1)
+    mlrs(joeNowX, joeNowY, -2, 1)
     -- beth
-    screen.move(bethNowX, bethNowY)
-    screen.line_rel(2, 2)
-    screen.stroke()
-    screen.move(bethNowX, bethNowY)
-    screen.line_rel(-2, 2)
-    screen.stroke()
+    mlrs(bethNowX, bethNowY, 2, 2)
+    mlrs(bethNowX, bethNowY, -2, 2)
     -- alex
-    screen.move(alexNowX, alexNowY)
-    screen.line_rel(2, -2)
-    screen.stroke()
-    screen.move(alexNowX, alexNowY)
-    screen.line_rel(-2, -2)
-    screen.stroke()
+    mlrs(alexNowX, alexNowY, 2, -2)
+    mlrs(alexNowX, alexNowY, -2, -2)
   end
 
   if birdFrame == 2 then
     -- joe
-    screen.move(joeNowX, joeNowY)
-    screen.line_rel(2, -2)
-    screen.stroke()
-    screen.move(joeNowX, joeNowY)
-    screen.line_rel(-2, -2)
-    screen.stroke()
+    mlrs(joeNowX, joeNowY, 2, -2)
+    mlrs(joeNowX, joeNowY, -2, -2)
     -- beth
-    screen.move(bethNowX, bethNowY)
-    screen.line_rel(2, 1)
-    screen.stroke()
-    screen.move(bethNowX, bethNowY)
-    screen.line_rel(-2, 1)
-    screen.stroke()
+    mlrs(bethNowX, bethNowY, 2, 1)
+    mlrs(bethNowX, bethNowY, -2, 1)
     -- alex
-    screen.move(alexNowX, alexNowY)
-    screen.line_rel(2, 2)
-    screen.stroke()
-    screen.move(alexNowX, alexNowY)
-    screen.line_rel(-2, 2)
-    screen.stroke()
+    mlrs(alexNowX, alexNowY, 2, 2)
+    mlrs(alexNowX, alexNowY, -2, 2)
   end
 
 end
@@ -470,63 +539,49 @@ end
 
 function drawWind()
 
+  if not playing then
+    return
+  end
+  
   screen.level(screenL)
 
   windFrame1 = frame % 20
   windFrame2 = frame % 13
 
   if math.random(0, 1) == 1 then
-    screen.move(windFrame1 + 80, 49)
-    screen.line_rel(1, 0)
-    screen.stroke()
+    mlrs(windFrame1 + 80, 49, 1, 0)
   end
 
   if math.random(0, 2) ~= 0 then
-    screen.move(windFrame2 + 10, 49)
-    screen.line_rel(1, 0)
-    screen.stroke()
+    mlrs(windFrame2 + 10, 49, 1, 0)
   end
 
   if math.random(0, 3) ~= 0 then
-    screen.move((windFrame1 * 2), 54)
-    screen.line_rel(1, 0)
-    screen.stroke()
+    mlrs((windFrame1 * 2), 54, 1, 0)
   end
 
   if math.random(0, 2) ~= 1 then
-    screen.move(((windFrame1 + 4) * 3), 54)
-    screen.line_rel(1, 0)
-    screen.stroke()
+    mlrs(((windFrame1 + 4) * 3), 54, 1, 0)
    end
 
   if math.random(0, 4) ~= 0 then
-    screen.move(((windFrame1 + 2) * 5) + 28, 54)
-    screen.line_rel(1, 0)
-    screen.stroke()
+    mlrs(((windFrame1 + 2) * 5) + 28, 54, 1, 0)
   end
 
   if math.random(0, 1) == 1 then
-    screen.move((windFrame2 * 2) + 48, 61)
-    screen.line_rel(1, 0)
-    screen.stroke()
+    mlrs((windFrame2 * 2) + 48, 61, 1, 0)
   end
 
   if math.random(0, 1) == 1 then
-    screen.move(((windFrame1 + 6) * 4) + 57, 61)
-    screen.line_rel(1, 0)
-    screen.stroke()
+    mlrs(((windFrame1 + 6) * 4) + 57, 61, 1, 0)
   end
 
   if math.random(0, 1) == 1 then
-    screen.move(((windFrame2 + 2) * 10) + 57, 61)
-    screen.line_rel(1, 0)
-    screen.stroke()
+    mlrs(((windFrame2 + 2) * 10) + 57, 61, 1, 0)
   end
 
   if math.random(0, 1) == 1 then
-    screen.move(((windFrame2 + 3) * 8) + 57, 61)
-    screen.line_rel(1, 0)
-    screen.stroke()
+    mlrs((((windFrame2 + 3) * 8) + 57), 61, 1, 0)
   end
 
 end
@@ -534,25 +589,19 @@ end
 
 
 function lightOne()
-  screen.move(62, 25)
-  screen.line_rel(1, 1)
-  screen.stroke()
+  mlrs(62, 25, 1, 1)
 end
 
 
 
 function lightTwo()
-  screen.move(65, 17)
-  screen.line_rel(1, 0)
-  screen.stroke()
+  mlrs(65, 17, 1, 0)
 end
 
 
 
 function lightThree()
-  screen.move(69, 23)
-  screen.line_rel(1, 1)
-  screen.stroke()
+  mlrs(69, 23, 1, 1)
 end
 
 
@@ -631,97 +680,47 @@ function drawLandscape()
   screen.level(screenL)
 
   -- antenna sides
-  screen.move(62, 52)
-  screen.line(66, 20)
-  screen.stroke()
-  screen.move(70, 53)
-  screen.line(66, 20)
-  screen.stroke()
+  mls(62, 52, 66, 20)
+  mls(70, 53, 66, 20)
 
   -- antenna horizontals
-  screen.move(64, 34)
-  screen.line_rel(3, 0)
-  screen.stroke()
-  screen.move(64, 39)
-  screen.line_rel(3, 0)
-  screen.stroke()
-  screen.move(64, 45)
-  screen.line_rel(3, 0)
-  screen.stroke()
+  mlrs(64, 34, 3, 0)
+  mlrs(64, 39, 3, 0)
+  mlrs(64, 45, 3, 0)
 
   -- antenna supports
-  screen.move(62, 52)
-  screen.line(70, 44)
-  screen.stroke()
-  screen.move(70, 52)
-  screen.line(62, 44)
-  screen.stroke()
-  screen.move(70, 44)
-  screen.line(63, 37)
-  screen.stroke()
+  mls(62, 52, 70, 44)
+  mls(70, 52, 62, 44)
+  mls(70, 44, 63, 37)
 
   -- antenna details
-  screen.move(65, 19)
-  screen.line_rel(2, 0)
-  screen.stroke()
-  screen.move(62, 30)
-  screen.line_rel(2,0)
-  screen.stroke()
-  screen.move(67, 28)
-  screen.line_rel(2, 0)
-  screen.stroke()
-  screen.move(62, 27)
-  screen.line_rel(1, 2)
-  screen.stroke()
-  screen.move(69, 25)
-  screen.line_rel(1, 2)
-  screen.stroke()
+  mlrs(65, 19, 2, 0)
+  mlrs(62, 30, 2, 0)
+  mlrs(67, 28, 2, 0)
+  mlrs(62, 27, 1, 2)
+  mlrs(69, 25, 1, 2)
 
   -- distant horizon
-  screen.move(0,48)
-  screen.line_rel(60, 0)
-  screen.stroke()
-  screen.move(72,48)
-  screen.line_rel(50, 0)
-  screen.stroke()
+  mlrs(0, 48, 60, 0)
+  mlrs(72, 48, 50, 0)
 
   -- second horizon
-  screen.move(1, 50)
-  screen.line_rel(1, 0)
-  screen.stroke()
-  screen.move(4, 50)
-  screen.line_rel(40, 0)
-  screen.stroke()
-  screen.move(46, 50)
-  screen.line_rel(9, 0)
-  screen.stroke()
-  screen.move(57, 50)
-  screen.line_rel(1, 0)
-  screen.stroke()
-  screen.move(74, 50)
-  screen.line_rel(40, 0)
-  screen.stroke()
-  screen.move(116, 50)
-  screen.line_rel(2, 0)
-  screen.stroke()
+  mlrs(1, 50, 1, 0)
+  mlrs(4, 50, 40, 0)
+  mlrs(46, 50, 9, 0)
+  mlrs(57, 50, 1, 0)
+  mlrs(74, 50, 40, 0)
+  mlrs(116, 50, 2, 0)
 
   -- third horizon
-  screen.move(5, 55)
-  screen.line_rel(3, 0)
-  screen.stroke()
-  screen.move(10, 55)
-  screen.line_rel(40, 0)
-  screen.stroke()
-  screen.move(55, 55)
-  screen.line_rel(20, 0)
-  screen.stroke()
-  screen.move(80, 55)
-  screen.line_rel(41, 0)
-  screen.stroke()
-
+  mlrs(5, 55, 3, 0)
+  mlrs(10, 55, 40, 0)
+  mlrs(55, 55, 20, 0)
+  mlrs(80, 55, 41, 0)
+  
   -- closest horizon
-  screen.move(23, 62)
-  screen.line_rel(62, 0)
-  screen.stroke()
+  mlrs(38, 62, 62, 0)
+  mlrs(102, 62, 5, 0)
+  mlrs(110, 62, 2, 0)
 
 end
